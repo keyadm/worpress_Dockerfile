c1b05f7ee75fcf946d1f9f51529a3fe07e8ddbab
sys.nguyentrungkien <48834837+keyadm@users.noreply.github.com>
2020-11-27 11:54:17 +0700

    Update entrypoint.sh

--- MYSQL/entrypoint.sh
+++ MYSQL/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "Starting mysql daemon"
+mysql -g 'daemon off;'
+
 mysql -e "CREATE DATABASE wordpress;
 CREATE USER 'kiennt1'@'localhost' IDENTIFIED BY 'caoml@2020';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'kiennt1'@'localhost';
@@ -9,6 +12,3 @@ FLUSH PRIVILEGES;
 EXIT; "
 
 mysql -e "DROP USER 'root'@'localhost';FLUSH PRIVILEGES;"
-
-echo "Starting mysql daemon"
-mysql -g 'daemon off;'
